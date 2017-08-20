41e50fc2bf4947fb4b558559176f7ad1912f76ff
sreenesh234@gmail.com <sreenesh234@gmail.com>
2017-08-20 15:35:34 -0400

    added a uilabel

--- Harman_iOS/ViewController.swift
+++ Harman_iOS/ViewController.swift
@@ -15,6 +15,9 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         
         self.view.backgroundColor = UIColor.blue
+        let label = UILabel(frame: CGRect(x: 10, y: 10, width: 100, height: 50))
+        label.backgroundColor = UIColor.yellow
+        self.view.addSubview(label)
     }
 
     override func didReceiveMemoryWarning() {
