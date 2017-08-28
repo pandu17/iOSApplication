e2bb7adf9f0c5d860204e8e37a590294eedc246b
navitha <navitha@Navithas-MacBook-Pro.local>
2017-08-27 19:49:30 -0700

    changes made in both vc

--- Harman_iOS/ViewController.swift
+++ Harman_iOS/ViewController.swift
@@ -13,6 +13,10 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.view.backgroundColor = UIColor.red
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +24,8 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func addition(){
+        
+    }
 }
 
